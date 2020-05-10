577055eaa9e45d3d566a28cf09e478d193a0850e
Sean Bailey <sean@spb.li>
2020-05-09 22:07:17 -0400

    figure out twitter authentication, thanks to twurl

--- app.py
+++ app.py
@@ -1,82 +1,90 @@
-import requests, time, sys
-
+import time, sys, requests
+import twitter
+from requests_html import HTMLSession
+from requests_oauthlib import OAuth1, OAuth1Session
 from authvars import *
 
-# TEST IF APP IS ALREADY AUTHORIZED
+def getNowPlaying():
 
-try:
-    ACCESS_TOKEN
-    REFRESH_TOKEN
-    tokens_exist = True
-except:
-    tokens_exist = False
+    try:
+        session = HTMLSession()
+        r = session.get('http://composer.nprstations.org/widgets/iframe/now.html?station=50e451b6a93e91ee0a00028e')
+        r.html.render()
+    except:
+        print("Error encountered with page render.\n")
+        session.close()
+        return {
+            'song': "",
+            'artist': "",
+            'program': ""
+        }
 
-# AUTHORIZATION WORKFLOW
-# will only run if tokens are not in `authvars.py`
+    s = r.html.find('li.whatson-songTitle', first=True)
+    a = r.html.find('li.whatson-songArtist', first=True)
+    p = r.html.find('a.whatson-programName', first=True).text
 
-if not tokens_exist:
-
-    # device authorization
-    device_params = {
-        'client_id': APP_ID,
-        'client_secret': APP_SECRET,
-        'scope': 'listening.readonly'
-    }
-
-    device_headers = {
-        'Accept': 'application/json'
-    }
-
-    device_result = requests.post('https://authorization.api.npr.org/v2/device', 
-        data=device_params, 
-        headers=device_headers).json()
-    
-    print()
-    print("Go to", device_result['verification_uri'], 
-        "in the next 30 seconds",
-        "and enter the code", device_result['user_code'])
-    print()
-
-    print('Sleeping for 30 seconds...', end='')
-    for x in range(0, 30):
-        print('.', end='')
-        time.sleep(1)
-    print('Waking up...')
-    print()
-
-    # token authorization
-    token_params = {
-        'grant_type': 'device_code',
-        'client_id': APP_ID,
-        'client_secret': APP_SECRET,
-        'code': device_result['device_code'],
-        'scope': 'listening.readonly'
-    }
-
-    token_headers = {
-        'Accept': 'application/json'
-    }
-
-    token_result = requests.post('https://authorization.api.npr.org/v2/token',
-        data=token_params,
-        headers=token_headers).json()
-
-    print('Adding tokens to authvars.py...')
-
-    # test if token results exist
-    if 'access_token' in token_result and 'refresh_token' in token_result:
-        with open('authvars.py', 'a') as authf:
-            authf.write("ACCESS_TOKEN = '" + token_result['access_token'] + "'")
-            authf.write("REFRESH_TOKEN = '" + token_result['refresh_token'] + "'")
-        ACCESS_TOKEN = token_result['access_token']
-        REFRESH_TOKEN = token_result['refresh_token']
-        print('Tokens added successfully.')
+    if s:
+        s = s.text
     else:
-        print('Token authorization failed with error', token_result['error'])
-        print(token_result['message'])
-        print()
-        sys.exit(0)
+        s = ""
 
-# NPR SONG FUNCTIONALITY
+    if a:
+        a = a.text
+    else:
+        a = ""
 
-# http://composer.nprstations.org/widgets/iframe/searchlist.html?station=50e451b6a93e91ee0a00028e&visible=searchlist
+    session.close()
+
+    return {
+        'song': s,
+        'artist': a,
+        'program': p
+    }
+
+def sendTweet(text):
+    if text == None or text == "":
+        return
+
+    print("Tweeting: " + text)
+
+    api = twitter.Api(consumer_key=WYEP_CONSUMER_KEY,
+                  consumer_secret=WYEP_CONSUMER_SECRET,
+                  access_token_key=WYEP_TOKEN,
+                  access_token_secret=WYEP_SECRET)
+    
+    s = api.PostUpdate(text)
+    print('Posted tweet "' + s.text + '".')
+
+
+    # twitter = OAuth1Session(TW_ACCESS_TOKEN,
+    #                         client_secret=TW_ACCESS_SECRET,
+    #                         resource_owner_key='penis',
+    #                         resource_owner_secret='man')
+
+    # url = 'https://api.twitter.com/1.1/statuses/update.json?status=' + requests.utils.quote(text)
+    # r = twitter.post(url)
+
+    # print(r.json())
+
+# command line arg will disable tweet on startup
+if (len(sys.argv) > 1):
+    curr_track = getNowPlaying()
+else:
+    curr_track = {
+        'song': "",
+        'artist': "",
+        'program': ""
+    }
+
+if __name__ == "__main__":
+    while True:
+        new_track = getNowPlaying()
+
+        if new_track['program'] != curr_track['program'] and new_track['program'] != "":
+            sendTweet("Coming up on WYEP: " + new_track['program'])
+            time.sleep(5)
+
+        if new_track != curr_track and new_track['song'] != "":
+            sendTweet("Now playing: " + new_track['song'] + " by " + new_track['artist'])
+            curr_track = new_track
+        time.sleep(15)
