86be93c233e1cd003a2aab1b4a51a42989eb22f9
Sean Bailey <sean@spb.li>
2020-05-09 23:29:52 -0400

    create util dir

--- twtest.py
+++
@@ -1,3 +0,0 @@
-from app import *
-
-sendTweet("Hi! I'm the WYEP Playlist Bot. About me: \n - created by @theonesean\n - inspired by @KEXPNowPlaying\n - written in Python\n - hosted by @HorrificDev\n - obsessed with @WYEP\n - source code available at https://github.com/theonesean/wyep-now-playing")
---
+++ util/twtest.py
@@ -0,0 +1,5 @@
+from app import *
+
+# sendTweet("Hi! I'm the WYEP Playlist Bot. About me: \n - created by @theonesean\n - inspired by @KEXPNowPlaying\n - written in Python\n - hosted by @HorrificDev\n - obsessed with @WYEP\n - source code available at https://github.com/theonesean/wyep-now-playing")
+
+sendTweet("Signing off for tonight! Will be back (for real) tomorrow.")
